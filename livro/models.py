48765c19e4dbae3f1a4df4de3e2ff600549d69c9
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-15 17:12:13 -0300

    Criando Model livros

---
+++ livro/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Livros(models.Model):
+    nome = models.CharField(max_length = 100)
+    autor = models.CharField(max_length = 30)
+    co_autor = models.CharField(max_length = 30, blank = True)
+    data_cadastro = models.DateField(default = date.today)
+    emprestado = models.BooleanField(default = False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Livro'
+
+    def __str__(self):
+        return self.nomeclass
+    
